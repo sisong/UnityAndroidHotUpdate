ec8bcffe53a8cd8d3fb552f053bb17fc33eb7cdb
sisong <sisong@gmail.com>
2019-08-22 09:02:22 +0800

    change strategy: hook part of the secondary loaded lib close #16

--- project_hook_unity_jni/src/hook_unity.cpp
+++ project_hook_unity_jni/src/hook_unity.cpp
@@ -9,11 +9,13 @@
 #include <fcntl.h>
 #include "../xHook/libxhook/jni/xhook.h"
 #include <android/log.h>
+#include <dlfcn.h> // dlopen
 #ifdef __cplusplus
 extern "C" {
 #endif
     static const char* kLibUnity="libunity.so";
-    #define _IsDebug 0
+    
+    #define _IsDebug 1
 #define LOG_INFO(tag,info)         { __android_log_print(ANDROID_LOG_INFO,\
                                         "HookUnity_LOG",tag "\n",info); }
 #define LOG_INFO2(tag,info0,info1) { __android_log_print(ANDROID_LOG_INFO,\
@@ -46,101 +48,107 @@ extern "C" {
         return (ret==0)&&((file_stat.st_mode&S_IFREG)!=0);
     }
     
-    #define _MAP_PATH_TO(src,dst,opath,errValue,need_isFindFile)    \
+    #define _MAP_PATH_TO(src,dst,opath,errValue,need_isFindFile,isCanMap)   \
+        int olen=strlen(opath);     \
+        if ((olen>=src##Len)&&(0==memcmp(opath,src,src##Len))   \
+            &&((opath[src##Len]=='\0')||(opath[src##Len]==kDirTag))){   \
+            if (dst##Len+(olen-src##Len)>kMaxPathLen)   \
+                { LOG_ERROR2("MAP_PATH() len %d %s",olen,opath);  return errValue; } \
+            isCanMap=1;             \
+            memcpy(_newPath,dst,dst##Len);              \
+            memcpy(_newPath+dst##Len,opath+src##Len,olen-src##Len+1);   \
+            if ((!need_isFindFile)||isFindFile(_newPath)){              \
+                opath=&_newPath[0]; \
+                LOG_DEBUG("MAP_PATH() to %s",opath);    \
+            }else{                  \
+                LOG_DEBUG("MAP_PATH() not found %s",_newPath);      \
+            } }
+
+    #define IS_MAPED_PATH(opath) (opath==&_newPath[0])
+    
+    #define MAP_PATH(opath,errValue)    \
         char _newPath[kMaxPathLen+1];   \
         if (g_isHookSuccess){           \
-            int olen=strlen(opath);     \
-            if ((olen>=src##Len)&&(0==memcmp(opath,src,src##Len))   \
-                &&((opath[src##Len]=='\0')||(opath[src##Len]==kDirTag))){   \
-                if (dst##Len+(olen-src##Len)>kMaxPathLen)   \
-                    { LOG_ERROR2("MAP_PATH() len %d %s",olen,opath);  return errValue; } \
-                memcpy(_newPath,dst,dst##Len);              \
-                memcpy(_newPath+dst##Len,opath+src##Len,olen-src##Len+1);   \
-                if ((!need_isFindFile)||isFindFile(_newPath)){              \
-                    opath=_newPath;     \
-                    LOG_DEBUG("MAP_PATH() to %s",opath);    \
-                }else{                  \
-                    LOG_DEBUG("MAP_PATH() failed to %s",_newPath);          \
-                } } }
-
-    #define MAP_PATH(opath,errValue) _MAP_PATH_TO(g_apkPath,g_newApkPath,opath,errValue,0)
+            int  _null_nouse=0;         \
+            _MAP_PATH_TO(g_apkPath,g_newApkPath,opath,errValue,0,_null_nouse);  }
+    
+    #define MAP_SO_PATH(opath,errValue) \
+        char _newPath[kMaxPathLen+1];   \
+        int  isSoDirCanMap=0;           \
+        if (g_isHookSuccess){           \
+            _MAP_PATH_TO(g_soDir,g_soCacheDir,opath,errValue,1,isSoDirCanMap); \
+            if (!IS_MAPED_PATH(opath)) {\
+                int  _null_nouse=0; \
+                _MAP_PATH_TO(g_apkPath,g_newApkPath,opath,errValue,0,_null_nouse); } }
 
     #define CHECK_FUNC(tag,checkedFunc,errValue) { \
-        if (checkedFunc==NULL) { LOG_ERROR(tag,"old func is NULL"); return errValue; } }
+        if (checkedFunc==NULL) { LOG_ERROR(tag,"old function is NULL"); return errValue; } }
 
     
     //stat
-    typedef int (* TFuncStat)(const char* path,struct stat *file_stat);
-    static TFuncStat old_stat = NULL;
     static int new_stat(const char* path,struct stat* file_stat){
+        const int errValue=-1;
         LOG_DEBUG("new_stat() %s",path);
-        CHECK_FUNC("new_stat() %s",old_stat,-1);
-        MAP_PATH(path,-1);
-        return old_stat(path,file_stat);
+        MAP_PATH(path,errValue);
+        return ::stat(path,file_stat);
     }
     
     //fopen
-    typedef FILE* (* TFuncFOpen)(const char* path,const char* mode);
-    static TFuncFOpen old_fopen = NULL;
     static FILE* new_fopen(const char* path,const char* mode){
+        FILE* const errValue=NULL;
         LOG_DEBUG2("new_fopen() %s %s",mode,path);
-        CHECK_FUNC("new_fopen() %s",old_fopen,NULL);
-        MAP_PATH(path,NULL);
-        return old_fopen(path,mode);
+        MAP_PATH(path,errValue);
+        return ::fopen(path,mode);
     }
     
     //open
-    typedef int (*TFuncOpen)(const char* path,int flags,...);
-    static TFuncOpen old_open = NULL;
     static int new_open(const char *path, int flags, ...){
-        mode_t mode = 0;
-        int is_mode = ((flags & O_CREAT) == O_CREAT) || ((flags & 020000000) == 020000000);
-        if (is_mode){
-            va_list args;
-            va_start(args, flags);
-            mode = (mode_t)(va_arg(args, int));
-            va_end(args);
-        }
+        const int errValue=-1;
         LOG_DEBUG2("new_open() %d %s",flags,path);
-        CHECK_FUNC("new_open() %s",old_open,-1);
-        MAP_PATH(path,-1);
-        return is_mode? old_open(path,flags,mode) : old_open(path,flags);
+        MAP_PATH(path,errValue);
+        
+        va_list args;
+        va_start(args,flags);
+        int result=::open(path,flags,args);
+        va_end(args);
+        return result;
     }
-    
-    #define MAP_SO_PATH(opath,errValue) _MAP_PATH_TO(g_soDir,g_soCacheDir,opath,errValue,1)
     
     //dlopen
-    typedef void* (*TFuncDlopen)(const char* path,int flags);
-    static TFuncDlopen old_dlopen = NULL;
+    static int hook_lib(const char* libPath);
     static void* new_dlopen(const char* path,int flags){
+        void* const errValue=NULL;
         LOG_INFO2("new_dlopen() %d %s",flags,path);
-        CHECK_FUNC("new_dlopen() %s",old_dlopen,NULL);
-        MAP_SO_PATH(path,NULL);
-        LOG_INFO2("old_dlopen() %d %s",flags,path);
+        MAP_SO_PATH(path,errValue);
         //chmod(path,0755);
-        return old_dlopen(path,flags);
+        void* result=::dlopen(path,flags);
+        if ((result!=errValue)&&isSoDirCanMap)
+            hook_lib(path);//NOTE: hook libli2cpp.so libmono*.so ... , ignore hook error and other path lib
+        return result;
     }
+    
 
-    #define HOOK(lib,symbol){ \
-        if (0!=xhook_register(lib,#symbol,(void*)new_##symbol,(void **)&old_##symbol)){ \
-            LOG_ERROR2("hook() failed to find function:%s in %s",#symbol,lib); return -1; }}
-    static int hook(){
+    #define HOOK(lib,errValue,symbol){ \
+        if (0!=xhook_register(lib,#symbol,(void*)new_##symbol,NULL)){ \
+            LOG_ERROR2("hook_lib() failed to find function:%s in %s",#symbol,lib); return errValue; } }
+
+    static int hook_lib(const char* libPath){
         //xhook_enable_debug(1);
-        
-        HOOK(kLibUnity,stat);
-        HOOK(kLibUnity,fopen);
-        HOOK(kLibUnity,open);
-        HOOK(kLibUnity,dlopen);
-        
-        if(0 != xhook_refresh(1)){
-            LOG_ERROR("hook() %s","failed to replace function");
-            return -1;
-        }
+        const int errValue=-1;
+        HOOK(libPath,errValue,stat);
+        HOOK(libPath,errValue,fopen);
+        HOOK(libPath,errValue,open);
+        HOOK(libPath,errValue,dlopen);
+        if(0 != xhook_refresh(0)){
+            LOG_ERROR("hook_lib() failed to hook %s",libPath);
+            return errValue; }
+        xhook_clear();
+        LOG_INFO("hook_lib() success to hook %s",libPath);
         return 0;
     }
     
-    #define _COPY_PATH(dst,src) {\
-        dst##Len=strlen(src);   \
+    #define _COPY_PATH(dst,src) {   \
+        dst##Len=strlen(src);       \
         if (dst##Len>kMaxPathLen)   \
             { LOG_ERROR("hook_unity_doHook() strlen("#src") %d",dst##Len); return; } \
         memcpy(dst,src,dst##Len+1); }
@@ -157,10 +165,9 @@ extern "C" {
         _COPY_PATH(g_soCacheDir,soCacheDir);
         
         isDoHook=(g_newApkPathLen>0)&&isFindFile(newApkPath);
-        if (!isDoHook){ LOG_INFO("hook_unity_doHook() %s","not doHook"); return; }
-        int ret=hook();
+        if (!isDoHook){ LOG_INFO("hook_unity_doHook() %s","not do hook"); return; }
+        int ret=hook_lib(kLibUnity);
         if (ret!=0) { exit(-1); return; }
-        LOG_INFO("hook_unity_doHook() %s","success to doHook");
         g_isHookSuccess=1;
     }
 
