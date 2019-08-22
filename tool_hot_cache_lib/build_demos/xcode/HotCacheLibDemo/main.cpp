682368054d9859d0a34dd1059bacdd7512b871f5
sisong <sisong@gmail.com>
2019-08-22 08:45:19 +0800

    init lib(changed .so files) cache

---
+++ tool_hot_cache_lib/build_demos/xcode/HotCacheLibDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HotCacheLibDemo
+//
+//  Created by housisong on 2019/8/20.
+//  Copyright © 2019 sisong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
